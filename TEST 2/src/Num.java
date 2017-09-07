03aeb6ba806b882c303768929482681754b4f006
arwinstanley <arwds2@gmail.com>
2017-09-07 10:14:27 -0400

    Add files via upload

---
+++ TEST 2/src/Num.java
@@ -0,0 +1,11 @@
+import java.util.Scanner
+public class Num {
+	public static int sum(int a, int b) {
+		return a+b; 
+	}
+	
+	public static void main(String[] args) {
+		Scanner kb = new Scanner (System.in);
+		System.out.printf(" ")
+	}
+}
